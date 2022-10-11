492afdf6a4635ca6ce975dd9d231b161a79c25ab
Md Abdur Rouf Likhon <mdabdurrouf.likhon@gmail.com>
2022-10-12 00:40:40 +0600

    start practice button update and fa icon added

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Course.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faArrowRight } from "@fortawesome/free-solid-svg-icons";
 const Course = ({ course }) => {
   const { id,name, total, logo } = course;
   return (
@@ -11,7 +13,7 @@ const Course = ({ course }) => {
       <div className="course-info">
         <h1>{name}</h1>
         <h5>Number of Questions: {total}</h5>
-        <Link className="btn btn-primary w-75" to={`/quiz/${id}`}>Start Now</Link>
+        <Link className="btn btn-primary w-75" to={`/quiz/${id}`}>Start Practice  <FontAwesomeIcon  style={{marginLeft:'5px'}} icon={faArrowRight} /> </Link>
       </div>
     </div>
   );
