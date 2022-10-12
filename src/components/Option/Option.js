74276a5ca5ce5516d9df55b4e000574942b2f90e
Md Abdur Rouf Likhon <mdabdurrouf.likhon@gmail.com>
2022-10-12 23:21:02 +0600

    final check for deploy

--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -1,10 +1,10 @@
 import React from "react";
-import "./Option.css";
+import './Option.css'
 const Option = ({ option, question,getAnswer }) => {
     
   return (
-    <div ClassName=''>
-      <p>
+    <div >
+      <p ClassName='single-option'>
         <input onClick={()=>getAnswer(option)} type="radio" name={question} id={option} value={option} />
         <label for={option} > {option}</label>
       </p>
